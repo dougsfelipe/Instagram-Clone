79716b5d05b18757de67f8d6536995fe1a0a23f6
dougsfelipe <dfcs@cin.ufpe.br>
2020-04-12 14:26:04 -0300

    Iniciando o Projeto

---
+++ api/server.js
@@ -0,0 +1,124 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    multiparty = require('connect-multiparty'),
+    mongodb = require('mongodb'),
+    objectId = require('mongodb').ObjectID;
+
+var app = express();
+
+// body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(multiparty());
+
+var port = 8080;
+
+app.listen(port);
+
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost',27017,{}),
+    {}
+);
+
+console.log('Servidor HTTP esta escutando na porta ' + port);
+
+app.get('/', function(req, res){
+    res.send({msg:'Olá'});
+});
+
+// POST(create)
+app.post('/api', function(req, res){
+
+    res.setHeader("Access-Control-Allow-Origin","*");
+    var dados = req.body;
+
+    res.send(dados);
+
+    /*
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.insert(dados, function(err, records){
+                if(err){
+                    res.json({'status': '0'});
+                }else{
+                    res.json({'status': '1'});
+                }
+                mongoclient.close();
+            });
+        });
+    }); */
+});
+
+// GET(create)
+app.get('/api', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find().toArray(function(err,results){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+// GET by ID
+app.get('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find(objectId(req.params.id)).toArray(function(err,results){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.status(200).json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+
+// PUT by ID
+app.put('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.update(
+                {_id : objectId(req.params.id)},
+                {$set : { titulo: req.body.titulo}},
+                {},
+                function(err, records){
+                    if(err){
+                        res.json(err);
+                    }else{
+                        res.json(records);
+                    }
+                    mongoclient.close();
+                }
+            );
+        });
+    });
+});
+
+
+
+// DELETE by ID
+app.delete('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.remove({ _id : objectId(req.params.id)}, function(err, records){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(records)
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+

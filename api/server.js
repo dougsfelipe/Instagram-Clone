c634cf0d9f9be8cb7e7d3a9ae45dd3e86dde6ad3
dougsfelipe <dfcs@cin.ufpe.br>
2020-04-15 02:12:50 -0300

    Remoção de comentarios feita

--- api/server.js
+++ api/server.js
@@ -161,17 +161,29 @@ app.put('/api/:id', function(req, res){
 
 // DELETE by ID
 app.delete('/api/:id', function(req, res){
+
+   
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
-            collection.remove({ _id : objectId(req.params.id)}, function(err, records){
-                if(err){
-                    res.json(err);
-                }else{
-                    res.json(records)
+            collection.update(
+                { },
+                { $pull : {
+                    comentarios: {id_comentario : objectId(req.params.id) }
+
+                          }
+                },
+                {multi:true},
+                function(err, records){
+                    if(err){
+                        res.json(err);
+                    }else{
+                        res.json(records);
+                    }
+                    mongoclient.close();
                 }
-                mongoclient.close();
-            });
+            );
         });
     });
+    
 });
 
